40a8507f5626bbf33fc16704a27abe798cba3b6f
Marthin Satrya Pasaribu <marthin.pasaribu@go-jek.com>
2021-11-04 16:47:18 +0700

    Add new export theme

--- tepung/lib/tepung.dart
+++ tepung/lib/tepung.dart
@@ -6,5 +6,14 @@ export 'package:tepung/src/button/tepung_button_state.dart';
 export 'package:tepung/src/button/tepung_button_subtype.dart';
 export 'package:tepung/src/button/tepung_primary_button.dart';
 
+export 'package:tepung/src/theme/color/tepung_color_token.dart';
+export 'package:tepung/src/theme/spacing/tepung_spacing_token.dart';
+export 'package:tepung/src/theme/typhography/tepung_text_theme.dart';
+export 'package:tepung/src/theme/typhography/tepung_typhography_token.dart';
+
 export 'package:tepung/src/theme/tepung_theme.dart';
 export 'package:tepung/src/theme/tepung_theme_data.dart';
+export 'package:tepung/src/theme/tepung_app.dart';
+export 'package:tepung/src/theme/tepung_material_token.dart';
+export 'package:tepung/src/theme/tepung_theme_data.dart';
+export 'package:tepung/src/theme/tepung_theme_mode.dart';
